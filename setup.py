1e41ccd558b05f3fe71a7d5716f67aab6688f67c
Vivek Singh <vivekkmr45@yahoo.in>
2017-07-09 00:22:59 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+      name = 'ifsccode',
+      packages = ['ifsccode'], # this must be the same as the name above
+      version = '0.1',
+      description = 'Command line tool for downloading ifsc code',
+      author = 'Vivek Singh',
+      author_email = 'vivekkmr45@yahoo.in',
+      url = 'https://github.com/viveksyngh/ifsc-code',
+      download_url='https://github.com/viveksyngh/ifsc-code/archive/0.1.tar.gz',
+      keywords = ['Bank IFSC code', 'IFSC Code', 'IFSC'],
+      classifiers = [],
+)
