65dd220749117561fecfb4e97e3328ea88313efb
Vivek Singh <vivekkmr45@yahoo.in>
2017-07-09 02:59:13 +0530

    added function to get called with import

--- ifsccode/ifsc_code.py
+++ ifsccode/ifsc_code.py
@@ -7,6 +7,17 @@ from generate_excel_files import download_excel_files
 from generate_json_files import generate_json_files
 
 
+def get_ifsc_code(out_dir, file_type="EXCEL", verbose=False):
+    if out_dir and  not os.path.exists(out_dir):
+        raise os.OSError("Output directory %s does not exists"%(out_dir))
+    else:
+        file_urls = parse_urls()
+        download_excel_files(file_urls, out_dir, verbose)
+        if file_type == 'JSON':
+            generate_json_files(file_urls, out_dir, verbose)
+    print "Done"
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--filetype", default="EXCEL", help="Output file type e.g. EXCEL/JSON")
@@ -16,12 +27,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
     file_type = args.filetype
     out_dir = args.outdir
-    
-    if out_dir and  not os.path.exists(out_dir):
-        print "No such directory"
-    else:
-        file_urls = parse_urls()
-        download_excel_files(file_urls, out_dir, args.verbosity)
-        if file_type == 'JSON':
-            generate_json_files(file_urls, out_dir, args.verbosity)
+
+    get_ifsc_code(out_dir, file_type, args.verbosity)
 
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
-requires = ["BeautifulSoup==3.2.1", 
+requires = ["beautifulsoup4==4.5.1", 
             "requests==2.12.4",
             "xlrd==1.0.0"],
 
 setup(
       name = 'ifsccode',
       packages = ['ifsccode'], # this must be the same as the name above
-      version = '0.3.1',
+      version = '0.3.2',
       description = 'Command line tool for downloading ifsc code',
       author = 'Vivek Singh',
       author_email = 'vivekkmr45@yahoo.in',
       url = 'https://github.com/viveksyngh/ifsc-code',
-      download_url='https://github.com/viveksyngh/ifsc-code/archive/0.3.1.tar.gz',
+      download_url='https://github.com/viveksyngh/ifsc-code/archive/0.3.2.tar.gz',
       keywords = ['Bank IFSC code', 'IFSC Code', 'IFSC'],
       classifiers = [],
       platforms = ['Any'],
